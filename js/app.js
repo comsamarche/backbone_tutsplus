0034bac6511aa2a522ba6390158547d3f0947680
Guinaudeau Christine <comsamarche@gmail.com>
2017-04-25 17:19:53 +0200

    initial commit

---
+++ js/app.js
@@ -0,0 +1,279 @@
+(function ($) {
+
+
+/*
+* -- DATAS
+*/
+    var contacts = [
+        { name: "Contact 1", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 2", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 3", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Contact 4", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Contact 5", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" },
+        { name: "Contact 6", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "colleague" },
+        { name: "Contact 7", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "friend" },
+        { name: "Contact 8", address: "1, a street, a town, a city, AB12 3CD", tel: "0123456789", email: "anemail@me.com", type: "family" }
+    ];
+
+
+/*
+* -- MODELS
+*/
+    var Contact = Backbone.Model.extend({
+        defaults:{
+            photo: "http://placehold.it/150x150"
+        }
+    });
+
+/*
+* -- COLLECTIONS
+*/
+    var Directory = Backbone.Collection.extend({
+        model: Contact
+    });
+
+/*
+* --VIEW
+*/ 
+    var ContactView = Backbone.View.extend({
+        tagName: "article",
+        className: "contact-container",
+        template:_.template($("#contactTemplate").html()),
+        editTemplate:_.template($("#contactEditTemplate").html()),
+
+        render:function(){
+            $(this.el).html(this.template(this.model.toJSON()));
+            return $(this.el);
+        },
+
+        events:{
+            "click button.edit":    "editContact",
+            "change select.type":   "addType",
+            "click button.save":    "saveEdits",
+            "click button.cancel":  "cancelEdit",
+            "click button.delete" : "deleteContact"
+        },
+
+        deleteContact : function(){
+             var removedType = this.model.get("type").toLowerCase();
+             this.model.destroy();
+             this.remove();
+            if(_.indexOf(directory.getTypes(), removedType) === -1){
+                $(directory.el).find("#filter select").children('[value="'+ removedType +'"]').remove();
+            }
+        },
+        editContact: function () {
+
+            $(this.el).html(this.editTemplate(this.model.toJSON()));
+
+            var select = $('<select />', {
+                    "class": "type"
+                });
+
+            _.each(directory.getTypes(), function(item){
+                var option = $("<option/>" , {
+                    value: item.toLowerCase(),
+                    text: item.toLowerCase()
+                }).appendTo(select);
+            });
+
+            var newOpt = $("<option />", {
+                html: "<em>Add new...</em>",
+                value: "addType"   
+            });
+            select.append(newOpt);            
+
+            this.select = select.val($(this.el).find("#type").val()).insertAfter($(this.el).find(".name"));
+         
+            $(this.el).find("input[type='hidden']").remove();
+        }, 
+
+
+        addType : function(){
+
+            if (this.select.val() === "addType") {
+                 this.select.remove();
+                $("<input />", {
+                    "class": "type"
+                }).insertAfter($(this.el).find(".name")).focus();
+            }
+        },
+
+        saveEdits: function (e) {
+            e.preventDefault();
+        
+            var formData = {};
+            var prev = this.model.previousAttributes();
+            $(e.target).closest("form").find("input:first-child").addClass("photo");
+         
+            $(e.target).closest("form").find("input").each(function () {
+                var el = $(this);
+                formData[el.attr("class")] = el.val();
+            });
+         
+            if (formData.photo === "") {
+                delete formData.photo;
+            }
+
+            this.model.set(formData);  
+            prev = this.model.previousAttributes();         
+         
+            this.render();
+         
+            if (prev.photo === "/img/placeholder.png") {
+                delete prev.photo;
+            }
+         
+            _.each(contacts, function (contact) {
+                if (_.isEqual(contact.name, prev.name)) {
+                    contacts.splice(_.indexOf(contacts, contact), 1, formData);
+                }
+            });
+           $(directory.el).find("#filter").find("select").remove().end().append(directory.createSelect());
+
+        },
+
+        cancelEdit: function () {
+            this.render();
+        }
+
+    });
+
+    var DirectoryView = Backbone.View.extend({
+        el: $(".contacts"),
+
+        initialize: function(){
+            this.collection = new Directory(contacts);
+            this.render();
+            this.createSelect();
+            this.on("change:filterType", this.filterByType, this);
+            this.collection.on("reset", this.render, this);
+            this.collection.on("add" , this.renderContact , this);
+            this.collection.on("remove", this.removeContact, this);
+        },
+
+        render : function(){
+            var that = this;
+            _.each(this.collection.models, function(item){
+                that.renderContact(item);
+            }, this);
+        },
+
+        renderContact: function(item){
+            var contactView = new ContactView({
+                model: item
+            })
+            $(this.el).find('.contacts-list').append(contactView.render());
+        },
+
+        getTypes: function(){
+            return _.uniq(this.collection.pluck("type"), false, function(type){
+                return type.toLowerCase();
+            });
+        },
+
+        createSelect : function(){
+            var filter = $(this.el).find('#filter'),
+                select = $("<select />", {
+                    html: '<option value="all">All</option>'
+                });
+
+            _.each(this.getTypes(), function(item){
+                var option = $("<option/>" , {
+                    value: item.toLowerCase(),
+                    text: item.toLowerCase()
+                }).appendTo(select);
+            });
+             console.log(this.getTypes());
+            return filter.append(select);
+        },
+
+        events: {
+            "change #filter select" : "setFilter",
+            "click #add" : "addContact",
+            "click #showForm" : "showForm"
+        }, 
+
+        setFilter: function(e){
+            this.filterType = e.currentTarget.value;
+            this.trigger('change:filterType');
+        }, 
+
+        filterByType : function(){
+            this.clearContact();
+            if(this.filterType === "all"){
+                 this.collection.reset(contacts);
+                 contactsRouter.navigate("filter/all");
+            }else{
+                 this.collection.reset(contacts, { silent: true});
+                 var filterType = this.filterType,
+                 filtered = _.filter(this.collection.models, function(item){
+                    return item.get("type").toLowerCase() === filterType;
+                 });
+                 this.collection.reset(filtered);
+                 contactsRouter.navigate("filter/" + filterType);
+            }
+        },
+        clearContact : function(){
+            $(this.el).find('.contact-container').remove();
+        },
+        addContact : function(e){
+
+            e.preventDefault();
+            var formData = {};
+            $('#addContact').children("input").each(function(index, el) {
+                if($(el).val() !==""){
+                    formData[el.id] = $(el).val();
+                }
+            });
+
+            contacts.push(formData);
+            if(_.indexOf(this.getTypes() , formData.type) === -1){
+                this.collection.add(new Contact(formData));
+                $(this.el).find("#filter").find("select").remove().end().append(this.createSelect());
+            }else{
+                this.collection.add(new Contact(formData));
+            }
+
+        },
+        removeContact: function (removedModel) {
+            console.log("je vais ici");
+            var removed = removedModel.attributes;
+         
+            if (removed.photo === "/img/placeholder.png") {
+                delete removed.photo;
+            }
+         
+            _.each(contacts, function (contact) {
+                if (_.isEqual(contact, removed)) {
+                    contacts.splice(_.indexOf(contacts, contact), 1);
+                }
+            });
+
+        }, 
+
+        showForm : function(){
+            $(this.el).find('#addContact').toggleClass('visible');
+        }
+
+    });
+
+    /*
+     * --ROUTER
+    */
+    var ContactsRouter = Backbone.Router.extend({
+        routes : {
+                "filter/:type" : "urlFilter"
+        },
+        urlFilter : function(type){
+                directory.filterType = type;
+                $(directory.el).find('#filter select').val(type);
+                directory.filterByType();                  
+        }
+    });
+
+    var directory = new DirectoryView();
+    var contactsRouter = new ContactsRouter();
+    Backbone.history.start();
+
+} (jQuery));
